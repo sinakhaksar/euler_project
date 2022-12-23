6254c76f53569f0ae5bf03c2863f56e5d40a137c
sina khaksar <85802550+sinakhaksar@users.noreply.github.com>
2022-12-24 01:12:04 +0330

    Add files via upload

---
+++ euler5.py
@@ -0,0 +1,32 @@
+
+import time
+start_time = time.time()
+n = 2520
+
+# it's better to start here than 0 ;  
+condition = True 
+listt = [11,13,14,16,17,18,19,20]
+# we only need to test the list 
+
+while condition :
+    counter = 0 
+    for i in listt :
+        if n % i == 0 :
+            counter += 1 
+        else : 
+            n +=1
+            break
+    if counter == len(listt) :
+        print('>>>',n)
+        condition = False
+        quit()
+
+print("--- %s seconds ---" % (time.time() - start_time))
+'''
+it takes a while to run this 
+so i just give you the answer 
+
+it's  232792560
+
+'''
+ 
